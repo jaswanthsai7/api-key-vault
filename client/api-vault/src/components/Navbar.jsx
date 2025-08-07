949c3274e0dfd023ce658db0e09def86ce2acf31
jsai <jsai@Cogitate.us>
2025-08-07 20:32:48 +0530

    fixed the hiding of link options

--- client/api-vault/src/components/Navbar.jsx
+++ client/api-vault/src/components/Navbar.jsx
@@ -1,20 +1,21 @@
 "use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useAuth } from "@/context/AuthContext";
 
 export default function Navbar() {
   const { isAuthenticated, isAdmin, logout } = useAuth();
+  const pathname = usePathname(); 
 
   return (
     <nav className="fixed top-0 left-0 right-0 h-14 flex items-center justify-between px-6 bg-white shadow z-50 border-b">
-      
       <Link href="/" className="flex items-center gap-2">
         <img
-          src="/window.svg" 
+          src="/window.svg"
           alt="API Vault Logo"
           width={28}
           height={28}
-          priority={"true"}
         />
         <span className="text-lg font-semibold text-gray-800 hidden sm:inline">API Vault</span>
       </Link>
@@ -37,12 +38,16 @@ export default function Navbar() {
           </>
         ) : (
           <>
-            <Link href="/login" className="text-sm hover:underline">
-              Login
-            </Link>
-            <Link href="/register" className="text-sm hover:underline">
-              Register
-            </Link>
+            {pathname !== "/login" && (
+              <Link href="/login" className="text-sm hover:underline">
+                Login
+              </Link>
+            )}
+            {pathname !== "/register" && (
+              <Link href="/register" className="text-sm hover:underline">
+                Register
+              </Link>
+            )}
           </>
         )}
       </div>
